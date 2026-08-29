211d2f6d72e5981a2e2ebd09ccc53fa1da7293f3
Piyush <156587759+piko4@users.noreply.github.com>
2026-08-29 23:32:52 +0530

    some fix

--- assignment_1/q3/q3.cpp
+++ assignment_1/q3/q3.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<iomanip>
+#include <iomanip>
 using namespace std;
 int main(int argc, char const *argv[])
 {
@@ -8,33 +8,50 @@ int main(int argc, char const *argv[])
                          {19.0, 27.5, 50.2}};
     int rows = sizeof(array) / sizeof(array[0]);
     int cols = sizeof(array[0]) / sizeof(array[0][0]);
-    double hot_room_temp=0.0;
-    int hot_room_floor_no,hot_room_no;
-    for(int i=0;i<rows;i++){
-        for (int j = 0; j < cols; j++)
-        {
-            if (array[i][j]>hot_room_temp)
-            {
-                hot_room_temp=array[i][j];
-                hot_room_floor_no=i+1;
-                hot_room_no=j+1;
-            }
-            
-        }
-        
-    }
-    //-----------display----------
-    cout<<"\t\troom1   room2   room3\n";
+    double hot_room_temp, avg_floor_temp[3], sum;
+    int hot_room_floor_no, hot_room_no, warning;
     for (int i = 0; i < rows; i++)
     {
-        cout << "Floor " <<i+1<< " : ";
+        sum = array[i][0] + array[i][1] + array[i][2];
+        avg_floor_temp[i] = sum / 3;
+
         for (int j = 0; j < cols; j++)
         {
-            cout<<"\t" <<fixed<<setprecision(1) << array[i][j];
+
+            if (array[i][j] > hot_room_temp)
+            {
+                hot_room_temp = array[i][j];
+                hot_room_floor_no = i + 1;
+                hot_room_no = j + 1;
+            }
+            if (array[i][j] >= 30.0)
+                warning++;
+        }
+    }
+    double highest_avg_room_temp;
+    int highest_avg_room_no;
+    for (int i = 0; i < 3; i++)
+    {
+        if (avg_floor_temp[i] > highest_avg_room_temp)
+        {
+            highest_avg_room_temp = avg_floor_temp[i];
+            highest_avg_room_no = i + 1;
+        }
+    }
+
+    //-----------display----------
+    cout << "\t\troom1   room2   room3\n";
+    for (int i = 0; i < rows; i++)
+    {
+        cout << "Floor " << i + 1 << " : ";
+        for (int j = 0; j < cols; j++)
+        {
+            cout << "\t" << fixed << setprecision(1) << array[i][j];
         }
         cout << endl;
     }
-    cout<<"Hottest Room  : Floor "<<hot_room_floor_no<<", Room "<<hot_room_no<<" -> "<<hot_room_temp;
-
+    cout << "Hottest Room  : Floor " << hot_room_floor_no << ", Room " << hot_room_no << " -> " << hot_room_temp << "\370C" << endl;
+    cout << "Hottest Floor : Floor " << highest_avg_room_no << " (avg " << highest_avg_room_temp << "\370C)" << endl;
+    cout << "Rooms at WARNING or above : " << warning;
     return 0;
 }
