0f449707ba7f7cedb6d82f708a43a8dbd599fc82
Piyush <156587759+piko4@users.noreply.github.com>
2026-09-02 09:48:58 +0530

    q3 added

---
+++ assignment_1/q4/sensor_monitor.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void display(int warn, int crit, int readings[], int random_count)
+{
+    int normal=0, warning=0, critical=0, shutdown=0;
+    for (int i = 0; i < random_count; i++)
+    {
+        // cout<<" "<<readings[i];
+        if (readings[i] >= 0 && readings[i] <= 29)
+            normal++;
+        else if (readings[i] >= warn && readings[i] <= crit)
+            warning++;
+        else if (readings[i] >= crit && readings[i] <= 60)
+            critical++;
+        else if (readings[i] >= 60)
+            shutdown++;
+    }
+    cout << "config : Warn=" << warn << " Critical=" << crit << " Readings= " << random_count << endl;
+    cout << "Results : Normal: " << normal << " Warning: " << warning << " Critical: " << critical << " Shutdown: " << shutdown << endl;
+}
+//-------------------------MAIN-------------------------------------------------
+int main(int argc, char const *argv[])
+{
+    srand(time(0));
+    int size = stoi(argv[3]);
+    int readings[size];
+    for (int i = 0; i < size; i++)
+    {
+        readings[i] = rand() % 70;
+        // cout << " " << readings[i];
+    }
+
+    display(stoi(argv[1]), stoi(argv[2]), readings, size);
+    return 0;
+}
